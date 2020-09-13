01d4faa8aa0b9bbce7e41f0a47238f079d170825
RajeshBharadwajG <rajeghankot@gmail.com>
2020-09-13 22:37:53 +0530

    Second commit

--- program1.sh
+++ program1.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+i=0
 echo "Hello World welcome to the Devops"
+echo "Modified done on" $(date +"%m-%d-%Y %k %M %r %Z")  
+echo "Modified done 2nd time  on" $(date +"%m-%d-%Y %k %M %r %Z")
+
